a469b326742f975ba1c8a25c77cc240bd6df95fc
Arthur Osipyan <arthurosipyan@outlook.com>
2020-03-03 21:23:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 
 def open_file():
@@ -42,6 +43,14 @@ def anonymous_func():
     print("Tails: ", flip_list.count('T'))
 
 
+def list_comprehensions_exercise():
+    print([x for x in [random.randint(1, 1001) for i in range(50)] if x % 9 == 0])
+
+
+def is_email(email_list):
+    print("Email Matches:", len(re.findall("[\w._%+-]{1,20}@[\w.-]{2,20}.[A-Za-z]{2,3}", email_list)))
+
+
 def main():
     choice = 0
     try:
@@ -49,6 +58,8 @@ def main():
                            "1. Open File\n"
                            "2. Functions as Objects\n"
                            "3. Anonymous Functions\n"
+                           "4. List Comprehensions\n"
+                           "5. Valid Email with Regex"
                            "Exercise: "))
     except ValueError:
         print("ERROR: Please enter a number...\n")
@@ -59,6 +70,11 @@ def main():
         print(change_list(is_it_odd, nums))
     elif choice == 3:
         anonymous_func()
+    elif choice == 4:
+        list_comprehensions_exercise()
+    elif choice == 5:
+        emails = "db@aol.com m@.com @apple.com db@.com"
+        is_email(emails)
 
 
 main()
